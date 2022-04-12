21b095a2a68fb11e5a229d6e29025ca1c6080cf1
Raik Nagel <raik.nagel@power.cloud>
2022-04-12 10:03:17 +0200

    add example project

---
+++ example/Demo-Auto-Tester/src/main/java/io/github/ransprd/demo/auto/tester/DemoAutoTester.java
@@ -0,0 +1,12 @@
+package io.github.ransprd.demo.auto.tester;
+
+/**
+ *
+ * @author ranSprd
+ */
+public class DemoAutoTester {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
